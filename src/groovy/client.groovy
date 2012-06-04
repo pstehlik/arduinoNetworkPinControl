aaa9a8058ef3517661e0d1a2303552c4c3fba6be
Philip Stehlik <philip.stehlik@gmail.com>
2012-06-03 18:05:30 -0700

    adding correct groovy code to prior commit
    sending command to trigger multiple pin changes

--- src/groovy/client.groovy
+++ src/groovy/client.groovy
@@ -1,15 +1,16 @@
+//setting 4 pins and their states
+//3 - 1, 4 - 1, 5 - 0, 6 - 1 
+data = "4031041050061Some Message String With Long Content".getBytes()
 addr = InetAddress.getByName("192.168.8.2")
 port = 10042
+packet = new DatagramPacket(data, data.length, addr, port)
 socket = new DatagramSocket()
+socket.setSoTimeout(4000) // block for no more than 30 seconds
+socket.send(packet)
 
-commands = [
-" 30",
-" 40",
-" 50",
-" 60"
-]
-commands.each{ singleCommand ->
-  data = singleCommand.getBytes()
-  packet = new DatagramPacket(data, data.length, addr, port)
-  socket.send(packet)
-}
+
+buffer = (' ' * 4096) as byte[]
+response = new DatagramPacket(buffer, buffer.length)
+socket.receive(response)
+s = new String(response.data, 0, response.length)
+println "Server said: '$s'"
