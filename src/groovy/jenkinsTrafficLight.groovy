d7af2620f0898c7eb80e92db0f157e40b6c92b11
Philip Stehlik <philip.stehlik@gmail.com>
2012-07-28 01:05:37 -0700

    allowing traffic light control to run in timed loop until interruption

--- src/groovy/jenkinsTrafficLight.groovy
+++ src/groovy/jenkinsTrafficLight.groovy
@@ -1,82 +1,88 @@
 @Grab(group='org.ccil.cowan.tagsoup', module='tagsoup', version='1.2' )
 
-def jenkinsUrl = "http://192.168.27.19:8080/"
-def arduinoIp = "192.168.26.231"
-def pins = ['02','03','04'] //g,y,r - order is important
+final def jenkinsUrl = "http://192.168.27.19:8080/view/Platform%20Trunk"
+final def arduinoIp = "192.168.26.231"
+final def pins = ['02','03','04'] //g,y,r - order is important
 
 //red has precedence over yellow
-def yellowWhenFailed = 1/10 //one out of ten failed (or more) -> traffic light turns yellow
-def redWhenFailed = 2/10    //two out of ten failed (or more) -> traffic light turns red 
+final def yellowWhenFailed = 1/10 //one out of ten failed (or more) -> traffic light turns yellow
+final def redWhenFailed = 2/10    //two out of ten failed (or more) -> traffic light turns red 
 
+final def checkInterval = 30 * 1000 //every 30 sec
 
-//set up the XML parsing
-def tagsoupParser = new org.ccil.cowan.tagsoup.Parser()
-def slurper = new XmlSlurper(tagsoupParser)
-def htmlParser = slurper.parse(jenkinsUrl)
+while(true){
+  println "Running check on Jenkins for traffic control at [${new Date()}]"
+  //set up the XML parsing
+  def tagsoupParser = new org.ccil.cowan.tagsoup.Parser()
+  def slurper = new XmlSlurper(tagsoupParser)
+  def htmlParser = slurper.parse(jenkinsUrl)
 
-//counters for failed/succeeded tests
-int failedTests = 0
-int totalTests = 0
-int disabledTests = 0
+  //counters for failed/succeeded tests
+  int failedTests = 0
+  int totalTests = 0
+  int disabledTests = 0
 
-//parse the test results from the list view of jenkins
-//tested on jenkins 1.426
-htmlParser.'**'.findAll{ 
-    it.@id == 'projectstatus'
-}*.children()[0].children().findAll{
-    it.name() == 'td'
-}.findAll{
-    it.children()[0].name() == 'img'
-}*.children()*.each {
-    if(it.@alt != "Disabled"){ totalTests++ } else { disabledTests++ }   
+  //parse the test results from the list view of jenkins
+  //tested on jenkins 1.426
+  htmlParser.'**'.findAll{ 
+      it.@id == 'projectstatus'
+  }*.children()[0].children().findAll{
+      it.name() == 'td'
+  }.findAll{
+      it.children()[0].name() == 'img'
+  }*.children()*.each {
+      if(it.@alt != "Disabled"){ totalTests++ } else { disabledTests++ }   
 
-    if(it.@alt == "Failed"){ failedTests++ }
+      if(it.@alt == "Failed"){ failedTests++ }
+  }
+
+  println "Enabled tests: [${totalTests}], disabled tests: [${disabledTests}], failed tests: [${failedTests}]"
+  if(totalTests <= 0){return}
+
+  def failureRatio = failedTests/totalTests
+  println "failureRatio: [${failureRatio}]"
+
+  // setting traffic lights
+  //default is all lights off
+  mainData = "3"
+  pins.each{
+    mainData += "${it}0"
+  }
+
+  //determining which pin status should be replaced by 1 (turn on the pin)
+  def repl = null
+  if (redWhenFailed > 0 && failureRatio >= redWhenFailed){
+    repl = 2  
+  } else if(yellowWhenFailed >0 && failureRatio >= yellowWhenFailed){
+    repl = 1
+  } else {
+   repl = 0
+  }
+
+  println "Turning pin [${pins[repl]}] on pos [${repl}] in config array on."
+
+  data = ''
+  if(repl != null){
+    data = mainData.replace("${pins[repl]}0","${pins[repl]}1")
+  } else {
+    data = mainData
+  }
+
+  //send the data for pin status to the listening arduino
+  println "sending " + data
+  data = data.getBytes()
+  addr = InetAddress.getByName(arduinoIp)
+  port = 10042
+  packet = new DatagramPacket(data, data.length, addr, port)
+  socket = new DatagramSocket()
+  socket.setSoTimeout(2000) // block for no more than 30 seconds
+  socket.send(packet)
+
+  buffer = (' ' * 4096) as byte[]
+  response = new DatagramPacket(buffer, buffer.length)
+  socket.receive(response)
+  s = new String(response.data, 0, response.length)
+  println "Server said: '$s'"
+  
+  sleep(checkInterval)
 }
-
-println "Enabled tests: [${totalTests}], disabled tests: [${disabledTests}], failed tests: [${failedTests}]"
-if(totalTests <= 0){return}
-
-def failureRatio = failedTests/totalTests
-println "failureRatio: [${failureRatio}]"
-
-// setting traffic lights
-//default is all lights off
-mainData = "3"
-pins.each{
-  mainData += "${it}0"
-}
-
-//determining which pin status should be replaced by 1 (turn on the pin)
-def repl = null
-if (redWhenFailed > 0 && failureRatio >= redWhenFailed){
-  repl = 2  
-} else if(yellowWhenFailed >0 && failureRatio >= yellowWhenFailed){
-  repl = 1
-} else {
- repl = 0
-}
-
-println "Turning pin [${pins[repl]}] on pos [${repl}] in config array on."
-
-data = ''
-if(repl != null){
-  data = mainData.replace("${pins[repl]}0","${pins[repl]}1")
-} else {
-  data = mainData
-}
-
-//send the data for pin status to the listening arduino
-println "sending " + data
-data = data.getBytes()
-addr = InetAddress.getByName(arduinoIp)
-port = 10042
-packet = new DatagramPacket(data, data.length, addr, port)
-socket = new DatagramSocket()
-socket.setSoTimeout(2000) // block for no more than 30 seconds
-socket.send(packet)
-
-buffer = (' ' * 4096) as byte[]
-response = new DatagramPacket(buffer, buffer.length)
-socket.receive(response)
-s = new String(response.data, 0, response.length)
-println "Server said: '$s'"
